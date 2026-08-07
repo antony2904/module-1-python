912cc0b2fef73cef82701ed25db007e600cb8ad0
antony chalissery <antonychalissery2k18@gmail.com>
2026-08-07 15:59:13 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,81 @@
+# x=5
+# print(type(x))
+
+# name="John"
+# print(type(name))
+# print(x == 5)
+
+
+# x=[1,2,3,4,5,6,7,8]
+# x.insert(7,20)
+
+
+# print(x)
+
+
+# def detail(x,y):
+  #  result=x+y
+ #   print(result)
+
+    
+#detail(15,345)
+
+#def addition(x,y):
+  #  sum = x+y
+   # print(f'result = {sum}')
+    
+#addition(34,56)
+
+#def addition(name,completed,age):
+ #   print(f'my name is  {name}\ni have completed my  {completed}\ni am {age}')
+    
+#addition('antony','engineering','23')
+
+#x = 16
+
+#if x == 18:
+ #   print("true")
+
+#else :
+ #   print("false")
+ 
+#lst=[15,16,17,18,19]
+
+#for i in lst:
+ #   print(i)
+    
+#for i in range(0,250,25):
+ #   print(i)
+ 
+ 
+#lst = [45,87,96,57,37,86,92]
+
+#new_lst=[]
+
+#for i in lst:
+ #   if i>75:
+  #      new_lst.append(i)
+#print(new_lst)
+    
+# list comprehension ( expression for i in iterable)
+
+#lst = [45,87,96,57,37,86,92]
+
+#new_lst=[]
+
+#for i in lst:
+  #  result =i* 3
+ #   new_lst.append(result)
+
+#print(new_lst)
+
+lst=[0,1,2,3,4,5,6,7,8,9,10]
+
+new_lst=[]
+for i in lst:
+    if i % 2 == 0:
+        new_lst.append(i)
+
+print(new_lst)
+
+
