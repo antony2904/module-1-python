5720f932dc4ea6bcc6e4031ad47a0fd8a605ec5c
antony chalissery <antonychalissery2k18@gmail.com>
2026-08-12 16:57:23 +0530

    added python programs

---
+++ recursionnn.py
@@ -0,0 +1,48 @@
+#recursion
+#def detail(name):
+#    return f'hello,{name}'
+    
+
+#print(detail('antony'))
+
+
+#factorial
+#def factorial(num):
+#    if num == 1:
+#        return 1
+#    else:
+#        return num *factorial(num-1)
+
+
+#print(factorial(5))
+
+# problem: replace even numbers with zero
+#lst = [2, 4, 5, 6, 8, 10]
+#new_lst=[]
+
+#for i in lst:
+#    if i % 2 == 0:
+#        new_lst.append(0)
+#    else:
+#        new_lst.append(i)
+#print(new_lst)
+
+
+       
+
+#Write a python program to find the largest number in a list 
+
+lst = [456,78,345,232.377,234,1000]
+
+largest_numb = lst[0]
+
+for i in lst:
+    if i > largest_numb:
+        largest_num = i
+
+print("Largest number is:", largest_numb)
+        
+
+
+
+
---
+++ staff.py
@@ -0,0 +1,59 @@
+staff={
+    "name":'arun',
+    "age":35,
+    "department":'computrt science'
+
+    }
+
+print(staff['department'])
+
+
+tple =(1,2,3,4,5)
+
+print(tple[4])
+
+sett = {1,2,3,4,5,6,7,7}
+
+print(sett)
+
+x=7
+y=9
+
+def detail():
+    x = 5
+    def number():
+        print(x)
+    number()
+    print (x)
+    print(y)
+
+detail()
+
+print(x)
+
+print(y)
+
+result = lambda x : x + 1
+
+print(result(10))
+
+addition = lambda x,y : x+y
+print(addition(12,18))
+
+result = lambda x : x**3
+print(result(8))
+
+
+
+
+lst=[1,2,3,4,5,6,7,7,8,2,1]
+
+new_lst=[]
+for i in lst:
+    if i not in new_lst:
+        new_lst.append(i)
+
+print( new_lst)
+
+
+
